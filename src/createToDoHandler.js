a45fe97d8f284fbd77c38ef0e3ae14fb64ae0ed0
Paul Rogers <progers999@hotmail.co.uk>
2020-10-26 12:12:06 +0000

    Create to do list button has been moved to its own function

---
+++ src/createToDoHandler.js
@@ -0,0 +1,19 @@
+
+//creates the add to do list button and handles it
+const createToDoHandler = (element) => {
+	const createIcon = document.createElement('i');
+	createIcon.setAttribute('class', 'fa fa-plus-circle');
+
+	const createToDo = document.createElement('div');
+	createToDo.setAttribute('class', 'create-to-do');
+
+	const message = document.createElement('p');
+	message.innerText = `Add an item`;
+
+	createToDo.append(createIcon, message);
+
+	element.append(createToDo);
+
+};
+
+export{createToDoHandler};
--- src/renderToDoList.js
+++ src/renderToDoList.js
@@ -1,29 +1,15 @@
 import{clearElement} from "./clearElement.js";
+import{createToDoHandler} from "./createToDoHandler.js";
 
 const renderToDoList = (project) => {
 	const projectTitle = document.querySelector(".project-title");
 	projectTitle.innerText = `${project.name}`;
 
-	//Maybe move rendercreatebutton to its own function?
-
 	const toDoContainer = document.querySelector(".to-dos");
 
 	clearElement(toDoContainer);
 
-	const createIcon = document.createElement('i');
-	createIcon.setAttribute('class', 'fa fa-plus-circle');
-
-	const createToDo = document.createElement('div');
-	createToDo.setAttribute('class', 'create-to-do');
-
-	const message = document.createElement('p');
-	message.innerText = `Add an item`;
-
-	createToDo.append(createIcon, message);
-
-	toDoContainer.append(createToDo);
-
-	console.log(createToDo);
+	createToDoHandler(toDoContainer);
 
 };
 
